ceaef1763114c84eb615cf4e3dbffc2cc6bb536b
PietroMarcatti <46227354+PietroMarcatti@users.noreply.github.com>
2021-05-13 17:18:18 +0200

    parte dispari

--- runtimeVisitor.cpp
+++ runtimeVisitor.cpp
@@ -17,7 +17,7 @@ antlrcpp::Any runtimeVisitor::visitId_list(pascalParser::Id_listContext *ctx) {
     }
     this->vars[varname] = 0;
     // Continua sul resto della lista di identificatori, se presente
-    if(tail != NULL) 
+    if(tail != NULL)
         return visitId_list(tail);
     return NULL;
 }
@@ -40,14 +40,26 @@ antlrcpp::Any runtimeVisitor::visitOut(pascalParser::OutContext *ctx) {
         // caso stampa intero
         int value = visitExpr(ctx->expr());
         cout << value << endl;
+    }else if(ctx->STRING() != NULL){
+        string value = visitExpr(ctx->STRING());
+        cout << value << endl;
     }
-    // TODO: implementare il caso stampa di una stringa 
+    
     return NULL;
 }
 
 antlrcpp::Any runtimeVisitor::visitIn(pascalParser::InContext *ctx) {
     // TODO: implementa la lettura dell'input da tastiera
-    // il metodo deve aggiornare il valore della variabile
+    string varname = ctx->ID()->getText();
+    // controllo che la variabile sia stata dichiarata
+    if(this->vars.find(varname) == this->vars.end()) {
+        cerr << "Error: Undefined variable '" << varname << "'" << endl;
+        exit(EXIT_FAILURE);
+    }
+    int value;
+    cin >> value;
+    vars[varname] = value;
+
     return NULL;
 }
 
@@ -58,14 +70,14 @@ antlrcpp::Any runtimeVisitor::visitBranch(pascalParser::BranchContext *ctx) {
     if(guard) {
         // se guardia vera, esegue ramo then
         visitCode_block(ctx->code_block(0));
-    } 
-    // TODO: implementa l'esecuzione del ramo else (se presente) quando la guardia è falsa 
+    }
+    // TODO: implementa l'esecuzione del ramo else (se presente) quando la guardia è falsa
     else
     {
         if(ctx->code_block(1)!=NULL)
          visitCode_block(ctx->code_block(1));
     }
-    
+
     return NULL;
 }
 
@@ -76,7 +88,7 @@ antlrcpp::Any runtimeVisitor::visitLoop(pascalParser::LoopContext *ctx) {
         visitSt_list(ctx->st_list());
         guard = visitGuard(ctx->guard());
     }while(guard == false);
-    
+
     return NULL;
 }
 
@@ -94,7 +106,7 @@ antlrcpp::Any runtimeVisitor::visitExpr(pascalParser::ExprContext *ctx) {
     } else if(ctx->NUMBER() != NULL) {
         // caso NUMBER
         string numtext = ctx->NUMBER()->getText();
-        value = std::stoi(numtext); 
+        value = std::stoi(numtext);
     } else if(ctx->PLUS() != NULL) {
         // caso expr + expr
         // calcolo il valore della prima expr
@@ -138,7 +150,7 @@ antlrcpp::Any runtimeVisitor::visitExpr(pascalParser::ExprContext *ctx) {
     } else if(ctx->expr().size() == 1) {
         // caso parentesi
         value = visitExpr(ctx->expr(0));
-    } 
+    }
     return value;
 }
 
@@ -182,12 +194,64 @@ antlrcpp::Any runtimeVisitor::visitGuard(pascalParser::GuardContext *ctx) {
         value = visitGuard(ctx->guard(0));
     }
 
-    return value; 
+    return value;
 }
 
 antlrcpp::Any runtimeVisitor::visitRelation(pascalParser::RelationContext *ctx) {
-    // TODO: implementa la valutazione di un confronto 
+    // TODO: implementa la valutazione di un confronto
     // il metodo ritorna true se il confronto è vero, false altrimenti
-    return true;
+    bool value = false;
+
+    if (ctx->LT() != NULL) {
+        // calcolo il valore della prima expr
+        int left = visitExpr(ctx->expr(0));
+        // calcolo il valore della seconda expr
+        int right = visitExpr(ctx->expr(1));
+        // il risultato finale è true se left < right
+        value = left < right;
+    }
+    else if (ctx->LEQ() != NULL) {
+        // calcolo il valore della prima expr
+        int left = visitExpr(ctx->expr(0));
+        // calcolo il valore della seconda expr
+        int right = visitExpr(ctx->expr(1));
+        // il risultato finale è true se left <= right
+        value = left <= right;
+    }
+    else if (ctx->EQ() != NULL) {
+        // calcolo il valore della prima expr
+        int left = visitExpr(ctx->expr(0));
+        // calcolo il valore della seconda expr
+        int right = visitExpr(ctx->expr(1));
+        // il risultato finale è true se left == right
+        value = left == right;
+    }
+    else if (ctx->NEQ() != NULL)
+    {
+        // calcolo il valore della prima expr
+        int left = visitExpr(ctx->expr(0));
+        // calcolo il valore della seconda expr
+        int right = visitExpr(ctx->expr(1));
+        // il risultato finale è true se left != right
+        value = left != right;
+    }
+    else if (ctx->GEQ() != NULL) {
+        // calcolo il valore della prima expr
+        int left = visitExpr(ctx->expr(0));
+        // calcolo il valore della seconda expr
+        int right = visitExpr(ctx->expr(1));
+        // il risultato finale è true se left >= right
+        value = left >= right;
+    }
+    else if (ctx->GT() != NULL) {
+        // calcolo il valore della prima expr
+        int left = visitExpr(ctx->expr(0));
+        // calcolo il valore della seconda expr
+        int right = visitExpr(ctx->expr(1));
+        // il risultato finale è true se left > right
+        value = left > right;
+    }
+    
+    return value;
 }
 
